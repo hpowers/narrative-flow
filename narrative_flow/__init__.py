44984595c70e96bf0ed6fac1c0ad1ef324d1c401
Hunter Powers <hunter@wiretheplanet.com>
2026-01-17 12:28:29 -0500

    chore(main): release 0.5.5

--- narrative_flow/__init__.py
+++ narrative_flow/__init__.py
@@ -36,7 +36,7 @@ from .models import (
 )
 from .parser import WorkflowParseError, parse_workflow
 
-__version__ = "0.5.4"  # x-release-please-version
+__version__ = "0.5.5"  # x-release-please-version
 
 __all__ = [
     # Main functions
